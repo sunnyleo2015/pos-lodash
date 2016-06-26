d5ecd86ab512a4f524638511e31959854c03189c
sunnyleo2015 <user.email>
2016-06-26 16:40:22 +0800

    use lodash to code getCount

--- src/main.js
+++ src/main.js
@@ -1,29 +1,25 @@
 //TODO: Please write code in this file.
-
-var list=[
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000001',
-    'ITEM000003-2',
-    'ITEM000005',
-    'ITEM000005',
-    'ITEM000005'
-];
-
-
 function printInventory(inputs) {
-    getCount(list);
 }
 
 //统计商品数量
 function getCount(inputs) {
-    var object = {};
-    var result = [];
+    var buyedItem = [];
     //计算每个选项的数量
+    _.forEach(inputs, function(n){
+        if(n.indexOf('-') != -1){
+            var itemName = n.split('-')[0];
+            var itemCount = n.split('-')[1];
+            for(var i=0; i< itemCount;i++){
+                buyedItem.push(itemName);
+            }
+        }else {
+            buyedItem.push(n);
+        }
 
-    for(var i=0; i<inputs.length; i++){
+    });
+    return buyedItem;
+    /*for(var i=0; i<inputs.length; i++){
         if(!inputs[i].indexof('-')){
             if(!object[inputs[i]]){
                 object[inputs[i]] = 1;
@@ -39,12 +35,12 @@ function getCount(inputs) {
         result.push({barcode: i,count: object[i]})
     }
     console.log('getCount: '+result);
-    return result;
+    return result;*/
 }
 
 //转换为具体的数值
 function changeToItems(itemCount, itemInfo) {
-    var result =[];
+    var result = [];
     for(var i=0; i<itemCount; i++){
         for(var j=0; j<itemInfo; j++){
             if(itemCount[i].barcode == itemInfo[j].barcode){
@@ -58,8 +54,14 @@ function changeToItems(itemCount, itemInfo) {
             }
         }
     }
-    console.log('changeToItem');
+    console.log('changeToItems');
     return result;
 }
 
-//
+//计算优惠
+function itemsPromotion(buyedItemInfo, promotionInfo){
+    var result = [];
+    //判断商品类型
+
+    //
+}
